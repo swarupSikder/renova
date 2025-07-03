2f1b3a2fff5052bbdc52bdd1dc8669a96fbf2522
swarupSikder <swarupfx20@gmail.com>
2025-07-03 13:21:17 +0600

    update

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-r!w6-3$=c8o4^se3)6+r62j#zv83qfdson+wq2pd-tr5-s42zr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
