2edfbd7ea2a12c90f156ecf476f82f65b1dd97a4
swarupSikder <swarupfx20@gmail.com>
2025-08-11 07:25:30 +0600

    error raised

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
